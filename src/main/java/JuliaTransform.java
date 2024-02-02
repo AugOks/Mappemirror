16c89ddde4a8d09082470da44e8aad9993b4ea69
augoks <augustdo@stud.ntnu.no>
2024-02-02 11:36:56 +0100

    fixed javadoc.

--- src/main/java/JuliaTransform.java
+++ src/main/java/JuliaTransform.java
@@ -2,11 +2,21 @@ public class JuliaTransform extends  Transform2D {
   Complex point;
   int sign;
 
+  /***
+   * Constructor.
+   * @param point a complex vector point.
+   * @param sign the sign of the complex vector.
+   */
   public JuliaTransform(Complex point, int sign){
     this.point = point;
     this.sign = sign;
   }
 
+  /**
+   * performs the julia transformation.
+   * @param point A point in a 2D vector space. //no idea if this is correct.
+   * @return a transformed 2D vector.
+   */
   public Vector2D transform(Vector2D point){
     return point;
     //TODO: add body for transform.
